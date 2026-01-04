c18314a4f0c0c989dc4b367828fac750156b3aa6
unknown <nicnik271@gmail.com>
2026-01-05 02:08:24 +0800

    use let

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-const PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Information Security started on port ${PORT}`);

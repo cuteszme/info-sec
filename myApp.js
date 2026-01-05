a7d3b7a8c42499c74c6f5ae54b3fc5b54c1ee257
unknown <nicnik271@gmail.com>
2026-01-05 14:04:57 +0800

    3001

--- myApp.js
+++ myApp.js
@@ -1,50 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-let PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Information Security started on port ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const PORT = process.env.PORT || 3001;
 
 
 
@@ -57,7 +15,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
